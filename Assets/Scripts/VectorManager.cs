fd159ddbe90d5019361ac0e4025419be0efea9df
安原祐二 <yuji@Yujis-MacBook-Pro-15inch-2.local>
2016-08-07 17:51:55 +0900

    original line renderer (not worked yet)

--- Assets/Scripts/VectorManager.cs
+++ Assets/Scripts/VectorManager.cs
@@ -5,21 +5,24 @@ public class VectorManager : MonoBehaviour {
 
 	public GameObject hand_a_go;
 	public GameObject hand_b_go;
-	public GameObject vector_a_go;
-	public GameObject vector_b_go;
-	public GameObject vector_add_go;
-	public GameObject vector_sub_go;
-	public GameObject vector_cross_go;
+	// public GameObject vector_a_go;
+	// public GameObject vector_b_go;
+	// public GameObject vector_add_go;
+	// public GameObject vector_sub_go;
+	// public GameObject vector_cross_go;
 
 	private Transform hand_transform_a_;
 	private Transform hand_transform_b_;
-	private VolumetricLines.VolumetricLineBehavior hand_line_a_;
-	private VolumetricLines.VolumetricLineBehavior hand_line_b_;
-	private VolumetricLines.VolumetricLineBehavior line_a_;
-	private VolumetricLines.VolumetricLineBehavior line_b_;
-	private VolumetricLines.VolumetricLineBehavior line_add_;
-	private VolumetricLines.VolumetricLineBehavior line_sub_;
-	private VolumetricLines.VolumetricLineBehavior line_cross_;
+	// private VolumetricLines.VolumetricLineBehavior hand_line_a_;
+	// private VolumetricLines.VolumetricLineBehavior hand_line_b_;
+	// private VolumetricLines.VolumetricLineBehavior line_a_;
+	// private VolumetricLines.VolumetricLineBehavior line_b_;
+	// private VolumetricLines.VolumetricLineBehavior line_add_;
+	// private VolumetricLines.VolumetricLineBehavior line_sub_;
+	// private VolumetricLines.VolumetricLineBehavior line_cross_;
+
+	private int test_line_id_;
+	private int hand_a_line_id_;
 
 	private Vector3 hand_offset = new Vector3(0f, -0.02f, 0.04f);
 
@@ -43,13 +46,22 @@ public class VectorManager : MonoBehaviour {
 	{
 		hand_transform_a_ = hand_a_go.transform;
 		hand_transform_b_ = hand_b_go.transform;
-		hand_line_a_ = hand_a_go.GetComponent<VolumetricLines.VolumetricLineBehavior>();
-		hand_line_b_ = hand_b_go.GetComponent<VolumetricLines.VolumetricLineBehavior>();
-		line_a_ = vector_a_go.GetComponent<VolumetricLines.VolumetricLineBehavior>();
-		line_b_ = vector_b_go.GetComponent<VolumetricLines.VolumetricLineBehavior>();
-		line_add_ = vector_add_go.GetComponent<VolumetricLines.VolumetricLineBehavior>();
-		line_sub_ = vector_sub_go.GetComponent<VolumetricLines.VolumetricLineBehavior>();
-		line_cross_ = vector_cross_go.GetComponent<VolumetricLines.VolumetricLineBehavior>();
+		// hand_line_a_ = hand_a_go.GetComponent<VolumetricLines.VolumetricLineBehavior>();
+		// hand_line_b_ = hand_b_go.GetComponent<VolumetricLines.VolumetricLineBehavior>();
+		// line_a_ = vector_a_go.GetComponent<VolumetricLines.VolumetricLineBehavior>();
+		// line_b_ = vector_b_go.GetComponent<VolumetricLines.VolumetricLineBehavior>();
+		// line_add_ = vector_add_go.GetComponent<VolumetricLines.VolumetricLineBehavior>();
+		// line_sub_ = vector_sub_go.GetComponent<VolumetricLines.VolumetricLineBehavior>();
+		// line_cross_ = vector_cross_go.GetComponent<VolumetricLines.VolumetricLineBehavior>();
+
+		test_line_id_ = UTJ.VolumetricLine.Instance.spawn(0.25f /* width */, UTJ.VolumetricLine.Type.None);
+		hand_a_line_id_ = UTJ.VolumetricLine.Instance.spawn(0.25f /* width */, UTJ.VolumetricLine.Type.None);
+	}
+
+	void OnDestroy()
+	{
+		UTJ.VolumetricLine.Instance.destroy(test_line_id_);
+		UTJ.VolumetricLine.Instance.destroy(hand_a_line_id_);
 	}
 
 	private float get_value(SteamVR_Controller.DeviceRelation device_id)
@@ -112,42 +124,64 @@ public class VectorManager : MonoBehaviour {
 				break;
 		}
 
-		hand_line_a_.StartPos = hand_offset;
-		hand_line_a_.EndPos = new Vector3(0f, 0f, (1f + right_value)*0.5f);
-		hand_line_b_.StartPos = hand_offset;
-		hand_line_b_.EndPos = new Vector3(0f, 0f, (1f + left_value)*0.5f);
+		UTJ.VolumetricLine.Instance.begin(0 /* front */);
+	    {
+			Vector3 head = new Vector3(0f, 0f, 10f);
+			Vector3 tail = new Vector3(0f, 10f, 10f);
+			UTJ.VolumetricLine.Instance.renderUpdate(0 /* front */,
+													 test_line_id_,
+													 ref head,
+													 ref tail);
+		}
+	    {
+			Vector3 head = hand_transform_a_.TransformPoint(hand_offset);
+			Vector3 tail = hand_transform_a_.TransformPoint(hand_offset + new Vector3(0f, 0f, (1f+right_value)*0.5f));
+			UTJ.VolumetricLine.Instance.renderUpdate(0 /* front */,
+													 hand_a_line_id_,
+													 ref head,
+													 ref tail);
+		}
+		UTJ.VolumetricLine.Instance.end();
 
-		var center = new Vector3(0f, 1f, 2f);
-		vector_a_go.transform.position = center;
-		vector_a_go.transform.rotation = hand_transform_a_.rotation;
-		vector_b_go.transform.position = center;
-		vector_b_go.transform.rotation = hand_transform_b_.rotation;
+		Debug.Log("aa");
+		UTJ.VolumetricLine.Instance.render(0 /* front */);
 
-		vector_add_go.SetActive(display_add_);
-		vector_sub_go.SetActive(display_sub_);
-		vector_cross_go.SetActive(display_cross_);
+		// hand_line_a_.StartPos = hand_offset;
+		// hand_line_a_.EndPos = new Vector3(0f, 0f, (1f + right_value)*0.5f);
+		// hand_line_b_.StartPos = hand_offset;
+		// hand_line_b_.EndPos = new Vector3(0f, 0f, (1f + left_value)*0.5f);
 
-		vector_add_go.transform.position = center;
-		vector_add_go.transform.rotation = Quaternion.identity;
-		vector_sub_go.transform.position = center;
-		vector_sub_go.transform.rotation = Quaternion.identity;
-		vector_cross_go.transform.position = center;
-		vector_cross_go.transform.rotation = Quaternion.identity;
+		// var center = new Vector3(0f, 1f, 2f);
+		// vector_a_go.transform.position = center;
+		// vector_a_go.transform.rotation = hand_transform_a_.rotation;
+		// vector_b_go.transform.position = center;
+		// vector_b_go.transform.rotation = hand_transform_b_.rotation;
 
-		var endpos_a = new Vector3(0f, 0f, 1f + right_value);
-		var endpos_b = new Vector3(0f, 0f, 1f + left_value);
-		line_a_.StartPos = Vector3.zero;
-		line_a_.EndPos = endpos_a;
-		line_b_.StartPos = Vector3.zero;
-		line_b_.EndPos = endpos_b;
-		line_add_.StartPos = Vector3.zero;
-		line_add_.EndPos = (vector_a_go.transform.TransformVector(endpos_a) +
-							vector_b_go.transform.TransformVector(endpos_b));
-		line_sub_.StartPos = Vector3.zero;
-		line_sub_.EndPos = (vector_a_go.transform.TransformVector(endpos_a) -
-							vector_b_go.transform.TransformVector(endpos_b));
-		line_cross_.StartPos = Vector3.zero;
-		line_cross_.EndPos = Vector3.Cross(vector_a_go.transform.TransformVector(endpos_a),
-										   vector_b_go.transform.TransformVector(endpos_b));
+		// vector_add_go.SetActive(display_add_);
+		// vector_sub_go.SetActive(display_sub_);
+		// vector_cross_go.SetActive(display_cross_);
+
+		// vector_add_go.transform.position = center;
+		// vector_add_go.transform.rotation = Quaternion.identity;
+		// vector_sub_go.transform.position = center;
+		// vector_sub_go.transform.rotation = Quaternion.identity;
+		// vector_cross_go.transform.position = center;
+		// vector_cross_go.transform.rotation = Quaternion.identity;
+
+		// var endpos_a = new Vector3(0f, 0f, 1f + right_value);
+		// var endpos_b = new Vector3(0f, 0f, 1f + left_value);
+		// line_a_.StartPos = Vector3.zero;
+		// line_a_.EndPos = endpos_a;
+		// line_b_.StartPos = Vector3.zero;
+		// line_b_.EndPos = endpos_b;
+		// line_add_.StartPos = Vector3.zero;
+		// line_add_.EndPos = (vector_a_go.transform.TransformVector(endpos_a) +
+		// 					vector_b_go.transform.TransformVector(endpos_b));
+		// line_sub_.StartPos = Vector3.zero;
+		// line_sub_.EndPos = (vector_a_go.transform.TransformVector(endpos_a) -
+		// 					vector_b_go.transform.TransformVector(endpos_b));
+		// line_cross_.StartPos = Vector3.zero;
+		// line_cross_.EndPos = Vector3.Cross(vector_a_go.transform.TransformVector(endpos_a),
+		// 								   vector_b_go.transform.TransformVector(endpos_b));
 	}
 }
---
+++ Assets/Scripts/VolumetricLineRenderer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UTJ {
+
+[RequireComponent(typeof(MeshFilter),typeof(MeshRenderer))]
+public class VolumetricLineRenderer : MonoBehaviour {
+
+	public Material material_;
+	private MeshFilter mf_;
+	private MeshRenderer mr_;
+
+	void Awake()
+	{
+		VolumetricLine.Instance.init(material_);
+	}
+
+	void Start()
+	{
+		mf_ = GetComponent<MeshFilter>();
+		mr_ = GetComponent<MeshRenderer>();
+		mf_.sharedMesh = VolumetricLine.Instance.getMesh();
+		mr_.sharedMaterial = VolumetricLine.Instance.getMaterial();
+		mr_.SetPropertyBlock(VolumetricLine.Instance.getMaterialPropertyBlock());
+	}
+}
+
+} // namespace UTJ {
